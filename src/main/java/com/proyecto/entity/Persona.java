b648f2a1983519bc5bcd304b81ae0674a3b98bf1
VictorJCruz <98476822+VictorJCruz@users.noreply.github.com>
2022-08-02 17:25:53 -0600

    Security
    Seguridad del proyecto

--- src/main/java/com/proyecto/entity/Persona.java
+++ src/main/java/com/proyecto/entity/Persona.java
@@ -30,6 +30,10 @@ public class Persona implements Serializable {
     private String telefono;
     private String email;
 
+    private String password;
+    private int active;
+    private String roles = "";
+    private String permisos = "";
 
     public long getId() {
         return id;
@@ -77,6 +81,53 @@ public class Persona implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getPermisos() {
+        return permisos;
+    }
+
+    public void setPermisos(String permisos) {
+        this.permisos = permisos;
+    }
+    
+      public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+    
+
+    public List<String> getPermisosList() {
+         if (this.permisos.length()>0){
+            return Arrays.asList(this.permisos.split(","));
+        }
+        return new ArrayList<>();
     }
 
  

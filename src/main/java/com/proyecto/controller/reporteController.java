2382578676e522f58ba15385a75f6393a16911e7
VictorJCruz <98476822+VictorJCruz@users.noreply.github.com>
2022-08-04 20:20:55 -0600

    Report

--- src/main/java/com/proyecto/controller/reporteController.java
+++ src/main/java/com/proyecto/controller/reporteController.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.proyecto.controller;
+
+import com.proyecto.service.PersonaService;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author vicjc
  */
+@RestController
 public class reporteController {
-    
+
+    @Autowired
+    private PersonaService personaservice;
+
+    @GetMapping("/Reporte Streamteg")
+    public String generatedPdf() throws FileNotFoundException, JRException{
+
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(personaservice.getAllPersona());
+        JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream("C:\\Users\\vicjc\\OneDrive\\Documentos\\GitHub\\ProyectoWP\\src\\main\\resources\\StreamtegCherry.jrxml"));
+
+        HashMap<String,Object> map=new HashMap<>();
+        JasperPrint report = JasperFillManager.fillReport(compileReport, map, beanCollectionDataSource);
+        JasperExportManager.exportReportToPdfFile(report,"ReporteStreamteg.pdf");
+
+        return "El reporte de Streamteg fue creado con exito";
+       
+    }
 }
